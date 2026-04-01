ab0470b4e6fdd258c3f47917d8a400a762b6f02f
VladProtsyshyn <bullgray.com@gmail.com>
2026-04-01 17:26:53 +0300

    feat: add base layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 import Navigation from './components/Navigation/Navigation';
 import HomePage from './pages/HomePage/HomePage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
@@ -11,13 +12,15 @@ function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/timer" element={<TimerPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/calculator" element={<CalculatorPage />} />
+          <Route path="/timer" element={<TimerPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
